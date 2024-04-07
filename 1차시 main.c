aa198591808a5fee11063d2279c229a5b2c6f5d6
Leeminjun21 <159463943+Leeminjun21@users.noreply.github.com>
2024-04-07 10:53:45 +0900

    Add files via upload

---
+++ 1차시 main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	/*
+	  첫번째 작성한 C 프로그램입니다.
+	  2024.04.04
+	*/
+	printf("Hello!   \n\n");
+	printf("I am a Student. \n");
+	printf("%d\n" ,1234);
+	printf("%d  %d \n", 10, 20);
+	return 1;
+}
+
+// 한줄 주석 LMJ
+
